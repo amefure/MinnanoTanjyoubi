83abb712e28ea4fa396b6bc6be0b06b3a0dc9a66
ame <shachi10969@gmail.com>
2022-09-29 21:28:20 +0900

    Initial Commit

---
+++ MinnanoTanjyoubi/MinnanoTanjyoubiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinnanoTanjyoubiApp.swift
+//  MinnanoTanjyoubi
+//
+//  Created by t&a on 2022/09/29.
+//
+
+import SwiftUI
+
+@main
+struct MinnanoTanjyoubiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
