57329d1e81f7e12d1ce29dac7769b6e868919bc2
amefure <shachi10969@gmail.com>
2025-05-17 12:47:29 +0900

    feat: Swift Testingの追加&objectVersionの修正(Xcode13 -> Xcode16)

---
+++ MinnanoTanjyoubiTests/MinnanoTanjyoubiTests.swift
@@ -0,0 +1,14 @@
+//
+//  MinnanoTanjyoubiTests.swift
+//  MinnanoTanjyoubiTests
+//
+//  Created by t&a on 2025/05/17.
+//
+
+import Testing
+
+struct MinnanoTanjyoubiTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
