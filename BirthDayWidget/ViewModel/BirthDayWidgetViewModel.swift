5f2ebf7a7d7580cd3d179e8eba1e56653e813e95
amefure <shachi10969@gmail.com>
2025-09-09 22:44:17 +0900

    ディレクトリ構成をグループ化&ViewModel基盤の実装

---
+++ BirthDayWidget/ViewModel/BirthDayWidgetViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BirthDayWidgetViewModel.swift
+//  MinnanoTanjyoubi
+//
+//  Created by t&a on 2025/09/09.
+//
+
+final class BirthDayWidgetViewModel {
+    
+    private let realmRepository = RealmRepository()
+    
+}
