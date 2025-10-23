8104fcc4a66d8bee6e545544673b56acddc9c5e1
amefure <shachi10969@gmail.com>
2025-10-23 22:48:56 +0900

    Domain&Dataディレクトへの移行

---
+++ MinnanoTanjyoubi/Domain/Models/InAppPurchase/SecretProductIdKey.swift
@@ -0,0 +1,13 @@
+//
+//  SecretProductIdKey.swift
+//  MinnanoTanjyoubi
+//
+//  Created by t&a on 2025/01/02.
+//
+
+enum SecretProductIdKey {
+    static let REMOVE_ADS = "com.ame.MinnanoTanjyoubi.remove_ads"
+    static let UNLOCK_STORAGE = "com.ame.MinnanoTanjyoubi.unlock_storage"
+    static let TEST_REMOVE_ADS = "com.test.remove_ads"
+    static let TEST_UNLOCK_STORAGE = "com.test.unlock_storage"
+}
