61c53fc3dd265642ab39c1e9a5101f615df11e30
amefure <shachi10969@gmail.com>
2025-11-14 23:24:16 +0900

    空のSecretCryptoKeyファイルを追加

---
+++ MinnanoTanjyoubi/Domain/Models/Common/SecretCryptoKey.swift
@@ -0,0 +1,13 @@
+//
+//  SecretCryptoKey.swift
+//  MinnanoTanjyoubi
+//
+//  Created by t&a on 2025/11/12.
+//
+
+import UIKit
+
+class SecretCryptoKey {
+    static let KEY = ""
+    static let IV = ""
+}
