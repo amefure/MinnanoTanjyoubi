ac2866e20c772c3f32a39bb39759fe5da4a85474
amefure <shachi10969@gmail.com>
2025-12-25 22:05:28 +0900

    画像を拡大・移動できるように実装

--- MinnanoTanjyoubi/Presentation/View/Dialog/ImagePreviewDialog.swift
+++ MinnanoTanjyoubi/Presentation/View/Dialog/ImagePreviewDialog.swift
@@ -11,6 +11,20 @@ struct ImagePreviewDialog: View {
     @Binding var isPresented: Bool
     let image: Image?
     let scheme: AppColorScheme
+    /// 拡大値
+    @State private var scale: CGFloat = 1.0
+    /// Viewを表示させる座標値
+    @State private var position = CGPoint(x: 0, y: 0)
+    /// ドラッグジェスチャー量
+    @GestureState private var dragOffset = CGSize.zero
+    /// 最小許容スケールサイズ
+    private let minScale: CGFloat = 1.0
+
+    private func reset() {
+        // 表示前にスケールを元に戻す
+        scale = 1.0
+        position = CGPoint(x: 0, y: 0)
+    }
 
     var body: some View {
         if isPresented {
@@ -25,15 +39,44 @@ struct ImagePreviewDialog: View {
                         .resizable()
                         .scaledToFit()
                         .padding(5)
-                        .frame(maxWidth: 300)
+                        .frame(maxWidth: DeviceSizeUtility.deviceWidth - 40)
                         .background(scheme.foundationSub)
                         .shadow(radius: 10)
                         .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .scaleEffect(scale)
+                        .offset(x: position.x + dragOffset.width, y: position.y + dragOffset.height)
+                        .gesture(
+                            SimultaneousGesture(
+                                MagnificationGesture()
+                                    .onChanged { value in
+                                        scale = max(value, minScale)
+                                    },
+                                DragGesture()
+                                    // ドラッグジェスチャーの変更を観測
+                                    .updating($dragOffset, body: { value, state, _ in
+                                        // 拡大していないなら移動を禁止する
+                                        guard scale != 1.0 else { return }
+                                        // state(GestureState)に移動した値を格納することで変化中も移動する
+                                        state = value.translation
+                                    })
+                                    // ドラッグ終了した際に呼ばれる
+                                    .onEnded { value in
+                                        // 拡大していないなら移動を禁止する
+                                        guard scale != 1.0 else { return }
+                                        // 移動が完了した座標を格納して固定
+                                        position.x += value.translation.width
+                                        position.y += value.translation.height
+                                    }
+                            )
+                        )
+
                     // 画面一杯にViewを広げる
                 }
             }.ignoresSafeArea()
                 .onTapGesture {
                     isPresented = false
+                }.onAppear {
+                    reset()
                 }
         }
     }
